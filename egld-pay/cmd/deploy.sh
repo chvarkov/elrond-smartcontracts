95ac29ca4aff27a53888a70694cbf01d21c0f266
chvarkov <chvarkov.alexey@gmail.com>
2022-09-14 23:31:01 +0000

    fix: fixed deploy script.

--- egld-pay/cmd/deploy.sh
+++ egld-pay/cmd/deploy.sh
@@ -1,7 +1,7 @@
 erdpy --verbose contract deploy \
-  --project=sc \
-  --pem="./wallets/root.pem" \
-  --gas-limit=800000000 \
+  --project=egld-pay/sc \
+  --pem="./egld-pay/wallets/root.pem" \
+  --gas-limit=80000000 \
   --proxy="https://testnet-gateway.elrond.com" \
-  --outfile="cmd/out/blogchain.json" \
+  --outfile="egld-pay/cmd/out/egld-pay.json" \
   --recall-nonce --send
